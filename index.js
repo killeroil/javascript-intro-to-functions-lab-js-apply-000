af9fbfc87bf14a033ebcbb33aacadb10dcd69baf
ZHENGJIANLIU <782059891@qq.com>
2020-07-19 18:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function whisper(string){
   return string.toLowerCase()
 }
 
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
