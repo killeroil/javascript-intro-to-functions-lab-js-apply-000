8aa6862793ae1eeac50f4287a902613c73593c67
ZHENGJIANLIU <782059891@qq.com>
2020-07-19 18:58:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function sayHiToGrandma(string){
   }else if(string == string.toUpperCase()){
     return "YES INDEED!"
   }else if(string == "I love you, Grandma."){
-    
+     return "I love you, too."
+  }else{
+    return "error!"
   }
 }
